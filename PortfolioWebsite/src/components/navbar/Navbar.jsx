63da8d08a87e2e130688d27d90bb26fce28883b7
Outimed404 <hadrien.abitbol@hotmail.fr>
2024-10-28 20:12:38 +0100

    Update Navbar.jsx

--- PortfolioWebsite/src/components/navbar/Navbar.jsx
+++ PortfolioWebsite/src/components/navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
         </motion.span>
                 <div className="social">
                     <a href="https://github.com/Outimed404" target="_blank" rel="noopener noreferrer"><img src="./github2.png" alt="githubpage"/> </a>
-                    <a href=" https://tryhackme.com/r/p/outimed" target="_blank" rel="noopener noreferrer"> <img src="THM.jpeg" alt="TryHackMePage"/>  </a>
+                    <a href=" https://tryhackme.com/r/p/Outimed404" target="_blank" rel="noopener noreferrer"> <img src="THM.jpeg" alt="TryHackMePage"/>  </a>
                     <a href="https://www.linkedin.com/in/hadrien-abitbol-3195b32a3/" target="_blank" rel="noopener noreferrer"> <img src="linkedin.png" alt="LinkdinPage"/>  </a>
 
                    
